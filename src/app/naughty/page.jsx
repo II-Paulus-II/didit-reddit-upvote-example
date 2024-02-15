99bf23b9f282d40e5f97cd6b8b1edb8a9a192848
II-Paulus-II <paulus.coding@gmail.com>
2024-02-15 11:40:43 +0000

    fix 2 - catch error for trying to upvote while not being logged in

---
+++ src/app/naughty/page.jsx
@@ -0,0 +1,10 @@
+import { LoginButton } from "@/components/LoginButton";
+
+export default async function Naughty () {
+  return (
+    <div className="max-w-screen-lg mx-auto p-4 mt-10">
+    Naughty Naughty You need to <LoginButton /> to do that
+    </div>
+  );
+}
+
--- src/components/Vote.jsx
+++ src/components/Vote.jsx
@@ -1,33 +1,44 @@
 import { db } from "@/db";
 import auth from "../app/middleware";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 import { VoteButton } from "./VoteButton";
 
 export async function Vote({ postId, votes }) {
   async function upvote() {
     "use server";
     const session = await auth();
-    console.log("Upvote", postId, "by user", session.user.id);
-    await db.query(
-      "INSERT INTO votes (user_id, post_id, vote, vote_type) VALUES ($1, $2, $3, $4)",
-      [session.user.id, postId, 1, "post"]
-    );
+    if(session) {
+      console.log("Upvote", postId, "by user", session.user.id);
+      await db.query(
+        "INSERT INTO votes (user_id, post_id, vote, vote_type) VALUES ($1, $2, $3, $4)",
+        [session.user.id, postId, 1, "post"]
+      );
 
-    revalidatePath("/");
-    revalidatePath(`/post/${postId}`);
+      revalidatePath("/");
+      revalidatePath(`/post/${postId}`);
+    }
+    else {
+      redirect("/naughty");
+    }
   }
 
   async function downvote() {
     "use server";
     const session = await auth();
-    console.log("Downvote", postId, "by user", session.user.id);
-    await db.query(
-      "INSERT INTO votes (user_id, post_id, vote, vote_type) VALUES ($1, $2, $3, $4)",
-      [session.user.id, postId, -1, "post"]
-    );
+    if(session) {
+      console.log("Downvote", postId, "by user", session.user.id);
+      await db.query(
+        "INSERT INTO votes (user_id, post_id, vote, vote_type) VALUES ($1, $2, $3, $4)",
+        [session.user.id, postId, -1, "post"]
+      );
 
-    revalidatePath("/");
-    revalidatePath(`/post/${postId}`);
+      revalidatePath("/");
+      revalidatePath(`/post/${postId}`);
+    }
+    else {
+      redirect("/naughty");
+    }
   }
 
   return (
